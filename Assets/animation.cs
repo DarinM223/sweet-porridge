0552eced390dc96441f3922658fc3329b7fd34a2
Darin Minamoto <darinm223@gmail.com>
2015-01-28 23:19:25 -0800

    Added new character models, raycasting script, and floor plan

--- Assets/animation.cs
+++ Assets/animation.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class animation : MonoBehaviour {
 
+	private RaycastHit hit;
+
 	// Use this for initialization
 	void Start () {
 
@@ -10,6 +12,12 @@ public class animation : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		// transform.Translate (transform.forward);
+		// Debug.DrawRay (transform.position, transform.forward * 100, Color.blue);
+		if (Physics.Raycast (transform.position, transform.forward, out hit, 100)) {
+			print ("There is a " + hit.transform.name + " directly ahead");
+		} else {
+			print ("There is nothing directly ahead");
+		}
 	}
 }
