9f33f2673d69daffa65fdb0965e4e38f6cab111c
Darin Minamoto <darinm223@gmail.com>
2015-01-28 23:47:42 -0800

    Added rotation and movement scripts and distance measurement

--- Assets/animation.cs
+++ Assets/animation.cs
@@ -4,20 +4,55 @@ using System.Collections;
 public class animation : MonoBehaviour {
 
 	private RaycastHit hit;
+	private GameObject oldWoman;
+	private GameObject pot;
+
+	private bool stoppedInFrontOfWoman;
+	private bool sawPot;
 
 	// Use this for initialization
 	void Start () {
-
+		oldWoman = GameObject.Find ("Old_Ass_Woman");
+		pot = GameObject.Find ("Pot");
+		stoppedInFrontOfWoman = false;
+		sawPot = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// transform.Translate (transform.forward);
-		// Debug.DrawRay (transform.position, transform.forward * 100, Color.blue);
+		var dist = Vector3.Distance (oldWoman.transform.position, transform.position);
+
 		if (Physics.Raycast (transform.position, transform.forward, out hit, 100)) {
-			print ("There is a " + hit.transform.name + " directly ahead");
+			if (hit.transform.name != "Terrain") {
+				if (hit.transform.name == "Pot") {
+					if (!sawPot) {
+						print ("Saw a " + hit.transform.name);
+						sawPot = true;
+					}
+				}
+			}
 		} else {
-			print ("There is nothing directly ahead");
+		}
+
+		// rotate to face old woman
+		Quaternion targetRotation;
+		if (stoppedInFrontOfWoman) {
+			targetRotation = Quaternion.LookRotation (pot.transform.position - transform.position);
+		} else {
+			targetRotation = Quaternion.LookRotation (oldWoman.transform.position - transform.position);
+		}
+//		targetRotation = Quaternion.LookRotation (oldWoman.transform.position - transform.position);
+		var str = Mathf.Min (1 * Time.deltaTime, 1);
+		transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, str);
+
+		// move until close to old woman
+		if (dist > 5) {
+			transform.Translate(transform.forward * -3 * Time.deltaTime);
+		} else {
+			if (!stoppedInFrontOfWoman) {
+				print ("Stopped in front of old woman!");
+				stoppedInFrontOfWoman = true;
+			}
 		}
 	}
 }
