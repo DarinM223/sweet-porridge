c691908f7927671ac217691edeebe0cbfb2b73af
Darin Minamoto <darinm223@gmail.com>
2015-02-06 16:49:51 -0800

    Started text event controller

---
+++ Assets/Scripts/DialogueTextController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogueTextController : MonoBehaviour {
+
+	void afterTitleCameraFinished() {
+		this.guiText.text = "HEY BUCKYY!!";
+	}
+
+	void OnEnable() {
+		TitleCameraController.OnFinishedZooming += afterTitleCameraFinished;
+	}
+
+	void OnDisable() {
+		TitleCameraController.OnFinishedZooming -= afterTitleCameraFinished;
+	}
+
+	// Use this for initialization
+	void Start () {
+		this.guiText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
