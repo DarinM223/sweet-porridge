e68d6789d61b4b426e79c8db3beb8035725f3626
Darin Minamoto <darinm223@gmail.com>
2015-03-07 19:22:30 -0800

    Started on HouseScene

---
+++ Assets/Scripts/HouseScene/DialogueTextController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace House
+{
+    public class DialogueTextController : MonoBehaviour
+    {
+    	public delegate void DialogueAction();
+    	public static event DialogueAction OnFinishedDialogue;
+    	public static event DialogueAction OnStartPot;
+
+    	private GameObject exclamationSign;
+    	private GameObject invisibleCube;
+
+    	private IEnumerator startDialogue()
+    	{
+    		this.guiText.text = "Girl: I brought home a pot that cooks an infinite amount of porridge";
+    		yield return new WaitForSeconds(3);
+    		this.guiText.text = "Girl: Now we will never get hungry again";
+    		yield return new WaitForSeconds(5);
+    		this.guiText.text = "Mom: ^_^";
+    		yield return new WaitForSeconds(4);
+    		this.guiText.text = "Girl: You start the pot by saying \"start, pot, start\" and you stop the pot with -";
+    		
+    		yield return new WaitForSeconds(2);
+    		exclamationSign.active = true;
+    		yield return new WaitForSeconds(3);
+
+    		this.guiText.text = "Girl: OMG Snow White just tweeted that she is awake I have to see brb lol";
+    		yield return new WaitForSeconds(3); 		
+
+    		if (OnFinishedDialogue != null) 
+    		{
+    			OnFinishedDialogue();
+    		}
+    	}
+
+    	private IEnumerator resumeDialogue()
+    	{
+    		this.guiText.text = "Mom: I guess I better start making the porridge";
+    		yield return new WaitForSeconds(3);
+    		this.guiText.text = "Mom: start, pot, start";
+    		yield return new WaitForSeconds(3);
+
+    		if (OnStartPot != null) 
+    		{
+    			OnStartPot();
+    		}
+
+    		yield return new WaitForSeconds(4);
+
+    		invisibleCube.SendMessage("FadeOut");
+    	}
+
+    	private void onSchoolgirlFinishedWalking() 
+    	{
+    		StartCoroutine(startDialogue());
+    	}
+
+    	void OnEnable() 
+        {
+			SchoolgirlController.OnFinishedWalking += onSchoolgirlFinishedWalking;
+        }
+
+        void OnDisable()
+        {
+        	SchoolgirlController.OnFinishedWalking -= onSchoolgirlFinishedWalking;
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+        }
+	
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
+
---
+++ Assets/Scripts/HouseScene/SchoolgirlController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace House 
+{
+	public class SchoolgirlController : MonoBehaviour 
+	{
+		private Animator animator;
+		public delegate void SchoolgirlAction();
+		public static event SchoolgirlAction OnFinishedWalking;
+		public static event SchoolgirlAction OnFinishedExiting;
+
+		private IEnumerator walkAway() 
+		{
+			yield return new WaitForSeconds(2);
+		}
+
+		private void onFinishedDialogue() 
+		{
+			StartCoroutine(walkAway());
+		}
+
+		void OnEnable()
+		{
+			DialogueTextController.OnFinishedDialogue += onFinishedDialogue;
+		}
+
+		void OnDisable()
+		{
+			DialogueTextController.OnFinishedDialogue -= onFinishedDialogue;
+		}
+
+		// Use this for initialization
+		void Start () 
+		{
+			animator = GetComponent<Animator>();
+
+		}
+		
+		// Update is called once per frame
+		void Update () 
+		{
+		
+		}
+	}
+}
