e79efe6e511167dcfef942045fc0e23f4ca31d9d
Darin Minamoto <darinm223@gmail.com>
2015-03-09 12:57:01 -0700

    Added water and npcs

---
+++ Assets/Scripts/VillageReturnScene/DoorController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace VillageReturn 
+{
+	public class DoorController : MonoBehaviour 
+	{
+
+		private GameObject invisibleBox;
+
+		private IEnumerator loadNewLevel()
+		{
+			invisibleBox.SendMessage("FadeOut");
+			// end level
+			yield return new WaitForSeconds(2);
+		}
+
+		void OnCollisionEnter(Collision col)
+		{
+			if (col.gameObject.name == "FPSGirl") 
+			{
+				StartCoroutine(loadNewLevel());
+			}
+		}
+
+		// Use this for initialization
+		void Start () 
+		{
+			invisibleBox = GameObject.Find("InvisibleBox");
+		}
+
+		// Update is called once per frame
+		void Update () 
+		{
+		
+		}
+	}
+}
+
+
